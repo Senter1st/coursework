5c95023b4a3f951b5ec89eacf0e34e039ad5e733
Vadim Chernyshov <senteris@yandex.ru>
2019-04-21 18:17:53 +0300

    Update configuration

--- src/main/resources/sql/createdb.sql
+++ src/main/resources/sql/createdb.sql
@@ -1,13 +1,15 @@
-create table Student(
-  idStudent     integer,
-  name          varchar,
-  score         float,
-  idSpecialty   integer,
-  foreign key(idSpecialty) references Specialty(idSpecialty)
+create table Specialty
+(
+    idSpecialty integer,
+    name        varchar,
+    description text
 );
 
-create table Specialty(
-  idSpecialty   integer,
-  name          varchar,
-  description   text
+create table Student
+(
+    idStudent   integer,
+    name        varchar,
+    score       float,
+    idSpecialty integer,
+    foreign key (idSpecialty) references Specialty (idSpecialty)
 );
